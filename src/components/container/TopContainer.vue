e1061b8e1a35b622bff0e1cfb9d2613ca56d912c
r.i <ri@TheLaughingMan.local>
2020-04-27 00:05:30 +0900

    update:基本構成作成

---
+++ src/components/container/TopContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <top-contents />
+</template>
+
+<script>
+import TopContents from '@/components/presentational/TopContents.vue'
+export default {
+  name: 'TopContainer',
+  components: {
+    TopContents
+  }
+}
+</script>
