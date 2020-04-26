13a00ec52bf70290f6426e0a13d06bddee8467d6
r.i <ri@TheLaughingMan.local>
2020-04-27 00:04:42 +0900

    update:i18n導入

---
+++ src/api/i18n.js
@@ -0,0 +1,11 @@
+import ja from '@/locales/ja.json'
+import enUS from '@/locales/en-US.json'
+
+const messages = {
+  ja: ja,
+  'en-US': enUS
+}
+
+export default {
+  messages
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,24 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueI18n from 'vue-i18n'
+
+import i18nUtils from './api/i18n.js'
 
 Vue.config.productionTip = false
+
+Vue.use(VueI18n)
+
+const messages = i18nUtils.messages
+
+const i18n = new VueI18n({
+  locale: 'ja',
+  messages: messages
+})
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
